ad98ff8eabd60a6747c9bf8fad844b1a43ae9848
Pranav <npv12@iitbbs.ac.in>
2021-03-14 00:31:19 +0530

    Plugin: silent download Moves all the information about the download to saved message so that the group isn't disturbed

--- noxx/plugins/files/download.py
+++ noxx/plugins/files/download.py
@@ -40,4 +40,36 @@ async def download(app: Noxx, message):
         except MessageIdInvalid:
             await message.reply_text('Download cancelled!')
 
+@Noxx.on_message(~filters.sticker & ~filters.via_bot & ~filters.edited & ~filters.forwarded & filters.me & filters.command(["silentdownload","sdl"], HANDLING_KEY))
+async def silent_download(app: Noxx, message):
+    home_dir = os.path.abspath(os.path.expanduser(' '.join(message.command[1:]) or './'))
+    saved_message = await app.send_message("me", 'Downloading')
+    await saved_message.edit("`Downloading`")
+    if os.path.isdir(home_dir):
+        home_dir = os.path.join(home_dir, 'Dumpster/')
+    downloadable_message = None
+    if(message.reply_to_message):
+        for i in POSSIBLE_MEDIA:
+            if getattr(message.reply_to_message, i, None):
+                home_dir = os.path.join(home_dir, f"{i}/")
+                downloadable_message = message.reply_to_message
+                break
+    else:
+        for i in POSSIBLE_MEDIA:
+            if getattr(message, i, None):
+                downloadable_message = message
+                break
+    await message.delete()
+    if(downloadable_message == None):
+        await saved_message.edit("This is not a valid media")
+    else:
+        try:
+            text = 'Downloading your file....\n'
+            home_dir = await downloadable_message.download(home_dir, progress=progress_callback, progress_args=(saved_message, text, False))
+            await saved_message.edit(f"Download complete. Saved to `{home_dir}`")
+            await asyncio.sleep(5)
+            await saved_message.delete()
+        except MessageIdInvalid:
+            await saved_message.reply_text('Download cancelled!')
+
         
