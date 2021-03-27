cb55bf5d3ea09a13b3f2c22e2898bd253d06316f
Pranav <npv12@iitbbs.ac.in>
2021-03-27 23:39:34 +0530

    Plugin: pin

---
+++ noxx/plugins/AdminRight/pin.py
@@ -0,0 +1,42 @@
+from pyrogram import Client, filters
+import asyncio
+from ...noxx import Noxx
+from ..constants import HANDLING_KEY
+
+@Noxx.on_message(~filters.sticker & ~filters.via_bot & ~filters.edited & ~filters.forwarded & filters.me & filters.command(["pin", "unpin"], HANDLING_KEY))
+async def pinmessages(app: Noxx, message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    command = f'{HANDLING_KEY}pin'
+
+    if message.chat.type not in ["supergroup", "channel", "group"]:
+        message_id = message.reply_to_message.message_id
+        if(message.command[0]==command):
+            await app.pin_chat_message(chat_id,message_id)
+        else:
+            await app.unpin_chat_message(chat_id,message_id)
+        await message.delete()
+        return
+
+    check_status = await app.get_chat_member(
+        chat_id=chat_id,
+        user_id=user_id
+    )
+
+    if message.reply_to_message:
+        if (check_status.status in ['creator', 'administrator']):
+            message_id = message.reply_to_message.message_id
+            if(message.command[0]==command):
+                await app.pin_chat_message(chat_id,message_id)
+            else:
+                await app.unpin_chat_message(chat_id,message_id)
+            await message.delete()
+            return
+        else:
+            await message.edit("You are not an admin here :(")
+            await asyncio.sleep(2)
+            await message.delete()
+    else:
+        await message.edit("Reply to a message to pin")
+        await asyncio.sleep(2)
+        await message.delete()
