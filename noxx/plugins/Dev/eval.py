2f432edbd8ff3e875d38894092eb29746b4b30c3
Pranav <npv12@iitbbs.ac.in>
2021-04-02 00:04:04 +0530

    Plugin: Eval
    *Thank you 
    https://github.com/okay-retard/ZectUserBot/blob/master/Zect/modules/dev.py

---
+++ noxx/plugins/Dev/eval.py
@@ -0,0 +1,69 @@
+import traceback
+import sys
+import os
+import re
+import subprocess
+from io import StringIO
+from pyrogram import filters
+
+from ...noxx import Noxx
+from ..constants import HANDLING_KEY
+
+async def aexec(code, client, message):
+    exec(
+        f"async def __aexec(client, message): "
+        + "".join(f"\n {l}" for l in code.split("\n"))
+    )
+    return await locals()["__aexec"](client, message)
+
+
+@Noxx.on_message(~filters.sticker & ~filters.via_bot & ~filters.edited & ~filters.forwarded & filters.me & filters.command("eval", HANDLING_KEY))
+async def evaluate(app:Noxx, message):
+    status_message = await message.reply_text("`Running ...`")
+    try:
+        cmd = message.text.split(" ", maxsplit=1)[1]
+    except IndexError:
+        await message.delete()
+        return
+    reply_to_id = message.message_id
+    if message.reply_to_message:
+        reply_to_id = message.reply_to_message.message_id
+    old_stderr = sys.stderr
+    old_stdout = sys.stdout
+    redirected_output = sys.stdout = StringIO()
+    redirected_error = sys.stderr = StringIO()
+    stdout, stderr, exc = None, None, None
+    try:
+        await aexec(cmd, app, message)
+    except Exception:
+        exc = traceback.format_exc()
+    stdout = redirected_output.getvalue()
+    stderr = redirected_error.getvalue()
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+    evaluation = ""
+    if exc:
+        evaluation = exc
+    elif stderr:
+        evaluation = stderr
+    elif stdout:
+        evaluation = stdout
+    else:
+        evaluation = "Success"
+    final_output = f"<b>Command:</b>\n<code>{cmd}</code>\n\n<b>OUTPUT</b>:\n<code>{evaluation.strip()}</code>"
+    if len(final_output) > 4096:
+        filename = "output.txt"
+        with open(filename, "w+", encoding="utf8") as out_file:
+            out_file.write(str(final_output))
+        await message.reply_document(
+            document=filename,
+            caption=cmd,
+            disable_notification=True,
+            reply_to_message_id=reply_to_id,
+        )
+        os.remove(filename)
+        await status_message.delete()
+    else:
+        await status_message.edit(final_output)
+
+#Thank you https://github.com/okay-retard/ZectUserBot/blob/master/Zect/modules/dev.py
