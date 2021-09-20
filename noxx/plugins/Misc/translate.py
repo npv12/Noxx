3c6ca11c680f31f975d113d1658daf792882bfcc
Pranav <npv12@iitbbs.ac.in>
2021-09-21 01:38:26 +0530

    translate: fix it
    * move to itrans
    * simplify codebase
    
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- noxx/plugins/Misc/translate.py
+++ noxx/plugins/Misc/translate.py
@@ -1,4 +1,4 @@
-from google_trans_new import google_translator
+from itranslate import itranslate as itrans
 from pyrogram import filters
 from ...noxx import Noxx
 import asyncio
@@ -7,23 +7,20 @@ from ..constants import HANDLING_KEY
 @Noxx.on_message(~filters.sticker & ~filters.via_bot & ~filters.edited & ~filters.forwarded & filters.me & filters.command(['tr','translate'], HANDLING_KEY))
 async def translate(app:Noxx, message):
     await message.edit("Translating...")
+    text = ''
+    lang = 'en'
     if message.reply_to_message and (message.reply_to_message.text or message.reply_to_message.caption):
-        if len(message.text.split()) == 1:
-            lang = 'en'
-        else:
+        # Detect which language to translate to. Defaults to english
+        if len(message.text.split()) != 1:
             lang = message.text.split()[1]
+        
+        # Detect the text for translation
         if message.reply_to_message.text:
             text = message.reply_to_message.text
         else:
             text = message.reply_to_message.caption
-        detected_language = google_translator().detect(text)
-        try:
-            translated_text = google_translator().translate(text, lang_tgt=lang)
-        except ValueError as err:
-            await message.edit(f"Error: `{str(err)}`")
-            await asyncio.sleep(2)
-            await message.delete()
-            return
+        
+        
     else:
         if len(message.text.split()) <= 1:
             await message.edit(f"Specify a language and a text in the order. Alternatively reply to a text")
@@ -32,15 +29,14 @@ async def translate(app:Noxx, message):
             return
         lang = message.text.split(None, 2)[1]
         text = message.text.split(None, 2)[2]
-        detected_language = google_translator().detect(text)
-        try:
-            translated_text = google_translator().translate(text, lang_tgt=lang)
-        except ValueError as error:
-            await message.edit(f"Unexpected error occured. Try again later")
-            await asyncio.sleep(2)
-            await message.delete()
-            print(error)
-            return
 
-    await message.edit(f"Translated from `{detected_language[0]}` to `{lang}`:\n```{translated_text}```")
+    try:
+        translated_text = itrans(text, to_lang=lang)
+    except ValueError as err:
+        await message.edit(f"Error: `{str(err)}`")
+        await asyncio.sleep(2)
+        await message.delete()
+        return
+
+    await message.edit(f"Translated to `{lang}`:\n```{translated_text}```")
     
